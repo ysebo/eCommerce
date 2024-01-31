0fd566598c4d7167aeb978dfd1a4d03cd222bce6
erdan <147805133+ysebo@users.noreply.github.com>
2024-01-31 14:33:41 +0100

    added a user

--- src/main/java/com/example/eCommerce/entities/User.java
+++ src/main/java/com/example/eCommerce/entities/User.java
@@ -1,11 +1,12 @@
 package com.example.eCommerce.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
 @Getter
@@ -13,6 +14,42 @@ import org.springframework.security.core.userdetails.UserDetails;
 @Table(name = "users_table")
 public class User implements UserDetails {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
