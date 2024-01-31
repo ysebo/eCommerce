055bf20549be516da28f6023cd9fd1ee8948badf
erdan <147805133+ysebo@users.noreply.github.com>
2024-01-31 14:54:58 +0100

    added a controller

--- src/main/java/com/example/eCommerce/controller/UserController.java
+++ src/main/java/com/example/eCommerce/controller/UserController.java
@@ -1,4 +1,11 @@
 package com.example.eCommerce.controller;
 
+import lombok.AllArgsConstructor;
+import net.minidev.json.JSONUtil;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
 public class UserController {
+
 }
